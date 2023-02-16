020bcd392833efff9d706f94e47143be410c3de0
Alex <baklan4ik1993@gmail.com>
2023-02-16 19:09:41 +0100

    fixes

--- src/components/somef.js
+++ src/components/somef.js
@@ -4,16 +4,12 @@ export const codeText = (str) => str.replace(/\n/g, "(/\\n/g)").replace(/\s/g, "
 export const decodeText = (str) => {
     str = str.replace(/\(\/\\n\/g\)/g, "\n").replace(/\(\/\\s\/g\)/g, " ")
     const textWithBreaks = str.split('\n').map((line, index) => {
-        return ( <
-            React.Fragment key = {
-                index
-            } > {
-                line
-            } <
-            br / >
-            <
-            /React.Fragment>
+        return (
+          <React.Fragment key={index}>
+            {line}
+            <br />
+          </React.Fragment>
         );
-    });
+      });
     return textWithBreaks
 }
